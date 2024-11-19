24bc531183e54de2ffd5399104a36e95267effd2
João Pedro Volante <joaopedrovolante@gmail.com>
2024-11-19 10:41:36 -0300

    proteção do reingresso via link e admin

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,4 @@ use App\Http\Controllers\evasaoController;
 
 Route::match(['get', 'post'], 'tabelaConsolidada', [evasaoController::class, 'tabelaConsolidada']);
 Route::get('/', [evasaoController::class, 'index']);
-Route::get('/reingresso', function() {
-    $reingresso = Reingresso::listarReingresso();
-    return view('reingresso', compact('reingresso'));
-});
+Route::match(['get', 'post'], 'reingresso', [evasaoController::class, 'reingresso']);
